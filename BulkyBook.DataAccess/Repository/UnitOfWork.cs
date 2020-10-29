950b1ad7c41eaac4dac197b8ed9b8e8018bbcc5e
Muhammad Ahmad Chand <ahmadchand420@gmail.com>
2020-10-29 17:58:20 +0500

    Adding Views

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -16,9 +16,11 @@ namespace BulkyBook.DataAccess.Repository
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            CoverType = new CoverTypeRepository(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public ISP_Call Sp_Call => throw new NotImplementedException();
