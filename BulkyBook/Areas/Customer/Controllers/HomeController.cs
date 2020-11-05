ee8925251727cb6db6962e286c5cc65994620be4
Muhammad Ahmad Chand <ahmadchand420@gmail.com>
2020-11-05 18:07:38 +0500

    Implement functionality of HOME SCREEN

--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System.Collections.Generic;
+using BulkyBook.Models;
 
 namespace BulkyBook.Areas.Customer.Controllers
 {
@@ -9,15 +12,18 @@ namespace BulkyBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
